a704735f9e82f9dc3457410e5f166bde99b1aa81
Alan Martin <alanadriel1302@gmail.com>
2024-11-08 13:27:39 -0300

    mejora2.0

--- funciones.py
+++ funciones.py
@@ -1,28 +1,55 @@
+import os
 def MenuOptions():
-    print("MENU DE GESTION DE PRODUCTOS")
-    print("\n 1. REGISTRO: Alta de nuevo productos")
+    print("\n\n------------------    MENU DE GESTION DE PRODUCTOS--------------    \n")
+    print("\n 1. REGISTRO: AGREGAR NUEVO PRODUCTO")
     #print("\n 2. Visualización: Consulta de datos de productos")
     #print("\n 3. Actualización: Modificar la cantidad en stock de los productos")
     #print("\n 4. Eliminacion: Dar de baja los productos ")
-    print("\n 2. Listado completo de los productos")
+    print("\n 2. LISTADO: MOSTRAR TODOS LOS PRODUCTOS")
     #print("\n 6. Reporte de Bajo Stock")
-    print("\n 3. Salir ")
-    nOption = int( input("\nPor favor, selecciona una opción (1-3): "))
-    while (nOption <=0  or nOption > 3 ):
-        print("\n Has Seleccinado un opcion invalida ")
-        nOption = int( input("\nPor favor, selecciona una opción (1-3): "))
-    print("\n Has seleccionado la opcion:",nOption,"\n")
-
+    print("\n 3. SALIR ")
+    while True:
+        try:
+            nOption = int( input("\nPor favor, selecciona una opción (1-3): "))
+            if 3>=nOption>=1 :
+                break
+            else:
+                print("Entrada inválida. Por favor, ingrese un número válido.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número válido.")
     return nOption 
 
 def fagregarProds(mi_lista):
-    nProduct = str(input("Ingrese el nombre del producto: "))
-    precioProd = float(input("Ingrese el precio del producto:") )
-    cantProduct = int(input("ingrese la cantidad del producto: "))
+    Agregando = True
+    while Agregando:
+        nProduct = input("Ingrese el nombre del producto: ")
+        while True:
+            try:
+                precioProd = float(input("Ingrese el precio del producto:") )
+                if type(precioProd)== float and precioProd > 0 :
+                    break
+            except ValueError:
+                print("\nIngrese un precio de producto correcto\n")
+        while True:
+            try:
+                cantProduct = int(input("ingrese la cantidad del producto: "))
+                if type(cantProduct)== int and cantProduct > 0:
+                    break
+            except ValueError:
+                print("\nIngrese una Cantidad Valida\n")
+        s_n = input("\n¿Desea Agregar mas productos ? (s/n): ")
+        if s_n == 'n':
+            Agregando = False
 
-    mi_lista.append([nProduct,precioProd,cantProduct])
 
+
+    
+
+    mi_lista.append({"Producto":nProduct,"Precio":precioProd, "Cantidad":cantProduct})
+    
 def fmostrarProductos(mi_lista):
-    for mi_lista in mi_lista:
-        print( "Descripción:", mi_lista[0], "Precio:", mi_lista[1],
-        "Cantidad:", mi_lista[2])
+    print("\n--------------Lista de Productos---------------- \n")
+    print("%-16s|%-13s|%-14s\n"%("PRODUCTOS","PRECIOS","CANTIDAD" ) )
+
+    for producto in mi_lista:
+       print("%-15s | $%10.2f | %6d" %(producto["Producto"],producto["Precio"],producto["Cantidad"] ) )
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import funciones 
+import os
 
-productosList=[ ["Mazanas", 12.00 , 30],
-                ["Peras", 9.50 , 15 ],
-                ["Tomate", 15.50, 50],
-                ["Zanahoria", 11.00 , 10],
-                ["Algo", 45.00 , 48 ] ]
+otraLista = [
+                {"Producto": "Manzanas" , "Precio": 500.25 , "Cantidad": 20 },
+                {"Producto": "Peras" , "Precio": 54654.5654 , "Cantidad": 3 },
+                {"Producto": "Tomate " , "Precio": 50.30, "Cantidad": 8000 },
+                {"Producto": "Zanahoria" , "Precio": 12.36 , "Cantidad": 5464 },
+                {"Producto": "Rucula" , "Precio": 10, "Cantidad": 13 }
+            ]
 
-
-
+os.system('cls')
 opcion = funciones.MenuOptions()
 while(opcion != 3):
     if(opcion == 1):
-        funciones.fagregarProds(productosList)
+        funciones.fagregarProds(otraLista)
         opcion = funciones.MenuOptions()
+        os.system('cls')
     elif(opcion == 2):
-        funciones.fmostrarProductos(productosList)
+        os.system('cls')
+        funciones.fmostrarProductos(otraLista)
         opcion = funciones.MenuOptions()
+
