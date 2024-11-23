47eb799056baec20088abc1308a6805c6442b2c3
Alan Martin <alanadriel1302@gmail.com>
2024-11-23 11:44:28 -0300

    funciones_listas

--- funciones.py
+++ funciones.py
@@ -1,55 +1,163 @@
-import os
 def MenuOptions():
     print("\n\n------------------    MENU DE GESTION DE PRODUCTOS--------------    \n")
-    print("\n 1. REGISTRO: AGREGAR NUEVO PRODUCTO")
-    #print("\n 2. Visualización: Consulta de datos de productos")
-    #print("\n 3. Actualización: Modificar la cantidad en stock de los productos")
-    #print("\n 4. Eliminacion: Dar de baja los productos ")
-    print("\n 2. LISTADO: MOSTRAR TODOS LOS PRODUCTOS")
-    #print("\n 6. Reporte de Bajo Stock")
-    print("\n 3. SALIR ")
+    print("%-30s\n%-30s\n%-30s\n%-30s\n%-30s\n%-30s\n%-30s"%("1. AGREGAR NUEVO PRODUCTO",
+                               "2. MOSTRAR LISTADO DE PRODUCTOS",
+                                "3. ACTUALIZAR PRODUCTO",
+                                 "4. ELIMINAR PRODUCTO",
+                                  "5. BUSCAR PRODUCTO",
+                                   "6. REPORTE BAJO STOCK" ,
+                                    "7. SALIR" ))
     while True:
         try:
-            nOption = int( input("\nPor favor, selecciona una opción (1-3): "))
-            if 3>=nOption>=1 :
-                break
+            opcion = int( input("\nPor favor, selecciona una opción (1-7): "))
+            if 1<= opcion <=7 : 
+                return opcion
             else:
-                print("Entrada inválida. Por favor, ingrese un número válido.")
+                print("Por favor, ingrese un número dentro del rango (1-7).") 
         except ValueError:
             print("Entrada inválida. Por favor, ingrese un número válido.")
-    return nOption 
-
+        
+     
+#RegistarProductos
 def fagregarProds(mi_lista):
     Agregando = True
+    print("--------------GESTION NUEVOS PRODUCTOS------------\n")
     while Agregando:
         nProduct = input("Ingrese el nombre del producto: ")
+        cat_prod = input("Ingrese la Categoria del producto:")
         while True:
             try:
                 precioProd = float(input("Ingrese el precio del producto:") )
-                if type(precioProd)== float and precioProd > 0 :
+                if precioProd > 0 :
                     break
             except ValueError:
                 print("\nIngrese un precio de producto correcto\n")
         while True:
             try:
                 cantProduct = int(input("ingrese la cantidad del producto: "))
-                if type(cantProduct)== int and cantProduct > 0:
+                if cantProduct > 0:
                     break
             except ValueError:
                 print("\nIngrese una Cantidad Valida\n")
+        ultima_clave = list(mi_lista.keys())[-1]
+        nueva_clave = ultima_clave+1
+        mi_lista[nueva_clave] = {"Producto":nProduct,"Precio":precioProd, "Cantidad":cantProduct,"Categoria":cat_prod,"Estado":True }
         s_n = input("\n¿Desea Agregar mas productos ? (s/n): ")
-        if s_n == 'n':
+        if s_n == 'n'or s_n == 'N':
             Agregando = False
 
-
-
-    
-
-    mi_lista.append({"Producto":nProduct,"Precio":precioProd, "Cantidad":cantProduct})
     
 def fmostrarProductos(mi_lista):
     print("\n--------------Lista de Productos---------------- \n")
-    print("%-16s|%-13s|%-14s\n"%("PRODUCTOS","PRECIOS","CANTIDAD" ) )
+    print("%-6s|%-16s|%-15s|%-14s|%-14s\n"%("CODIGO","PRODUCTOS","PRECIOS","CANTIDAD","CATEGORIA") )
 
-    for producto in mi_lista:
-       print("%-15s | $%10.2f | %6d" %(producto["Producto"],producto["Precio"],producto["Cantidad"] ) )
+    for codigo, producto in mi_lista.items():
+       if producto["Estado"] == True :
+            print("%-6d|%-16s | $%15.2f | %14d | %14s" %(codigo,producto["Producto"],producto["Precio"],
+                                            producto["Cantidad"],producto["Categoria"] ) )
+            
+def factualizar_Producto(diccionario):
+    actu = True
+    while actu:
+        try:
+            codProd = int(input("ingrese el Codigo  del producto: "))
+            Factualizacion(diccionario[codProd])
+            actu = False
+        except ValueError:
+                print("\nIngrese un codigo Valido\n")
+        except KeyError:
+            print("\n NO EXISTE UN PRODCUTO CON ESE CODIGO, POR FAVOR INGRESE UN CODIGO VALIDO")
+        except type(codProd)== int and codProd > 0:
+            print("\nIngrese un codigo Valido\n")
+
+    
+def Factualizacion(producto):
+    print("---------------CAMPOS QUE DESEA ACTUALIZAR------------------\n ")
+    print("1. NOMBRE PRODUCTO   \n")
+    print("2. PRECIO  \n")
+    print("3. CANTIDAD  \n")
+    print("4. CATEGORIA  \n")
+    print("5. SALIR \n")
+    while True:
+        try:
+            opcion = int(input("\nSelecciona una opción: (5 para salir)"))
+            if opcion == 1:
+                nuevo_nombre = input("Ingresa el nuevo nombre del producto: ")
+                producto["Producto"] = nuevo_nombre
+                print("Nombre actualizado correctamente.\n")
+            elif opcion == 2:
+                nuevo_precio = float(input("Ingresa el nuevo precio: "))
+                producto["Precio"] = nuevo_precio
+                print("Precio actualizado correctamente.\n")
+            elif opcion == 3:
+                nueva_cantidad = int(input("Ingresa la nueva cantidad: "))
+                producto["Cantidad"] = nueva_cantidad
+                print("Cantidad actualizada correctamente.\n")
+            elif opcion == 4:
+                nueva_categoria = input("Ingresa la nueva categoría: ")
+                producto["Categoria"] = nueva_categoria
+                print("Categoría actualizada correctamente.\n")
+            elif opcion == 5:
+                print("Saliendo sin realizar cambios.\n")
+                break
+            else:
+                print("Opción no válida. Intenta de nuevo.")
+        except ValueError:
+            print("Entrada no válida. Por favor, selecciona una opción válida.")
+        except opcion<0:
+             print("Entrada no válida. Por favor, selecciona una opción válida.")
+
+def fEliminar_producto(diccionario):
+    actu = True
+    while actu:
+        try:
+            codProd = int(input("ingrese el Codigo  del producto: "))
+            if type(codProd)== int and codProd > 0:
+                    if diccionario[codProd]["Estado"] != False: 
+                        diccionario[codProd]["Estado"]=False
+                        print("\n PRODUCTO ELIMINADO CON EXITO !")
+                        actu = False
+                    else:
+                        print("\n NO EXISTE UN PRODCUTO CON ESE CODIGO, POR FAVOR INGRESE UN CODIGO VALIDO")
+        except ValueError:
+                print("\nIngrese un codigo Valido\n")
+        except KeyError:
+            print("\n NO EXISTE UN PRODCUTO CON ESE CODIGO, POR FAVOR INGRESE UN CODIGO VALIDO")
+
+def fBuscar_producto(diccionario):
+    actu = True
+    while actu:
+        try:
+            codProd = int(input("ingrese el Codigo  del producto: "))
+            if type(codProd)== int and codProd > 0:
+                    if diccionario[codProd]["Estado"] != False: 
+                         
+                        print("\n%-6s|%-16s|%-15s|%-14s|%-14s"%("CODIGO","PRODUCTO","PRECIO","CANTIDAD","CATEGORIA") )
+                        print("%-6d|%-16s | $%15.2f | %14d | %14s" %(codProd,
+                                            diccionario[codProd]["Producto"],
+                                            diccionario[codProd]["Precio"],
+                                            diccionario[codProd]["Cantidad"],
+                                            diccionario[codProd]["Categoria"] ) )
+                        actu = False
+                    else:
+                        print("\n NO EXISTE UN PRODCUTO CON ESE CODIGO, POR FAVOR INGRESE UN CODIGO VALIDO")
+        except ValueError:
+                print("\nIngrese un codigo Valido\n")
+        except KeyError:
+            print("\n NO EXISTE UN PRODCUTO CON ESE CODIGO, POR FAVOR INGRESE UN CODIGO VALIDO")
+
+def fReporte_bajo_stock(mi_lista,limite):
+    print("\n--------------PRODUCTOS CON BAJO STOCK---------------- \n")
+    print("%-6s|%-16s|%-15s|%-14s|%-14s\n"%("CODIGO","PRODUCTOS","PRECIOS","CANTIDAD","CATEGORIA") )
+    BAND =0
+    for codigo, producto in mi_lista.items():
+       if producto["Estado"] == True and producto["Cantidad"]< limite :
+            BAND =1
+            print("%-6d|%-16s | $%15.2f | %14d | %14s" %(codigo,producto["Producto"],producto["Precio"],
+                                            producto["Cantidad"],producto["Categoria"] ) )
+    
+    if BAND ==0:
+        print("\n NO HAY PRODUCTOS CON BAJO STOCK !!!! ")
+
+
+
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import funciones 
 import os
 
-otraLista = [
-                {"Producto": "Manzanas" , "Precio": 500.25 , "Cantidad": 20 },
-                {"Producto": "Peras" , "Precio": 54654.5654 , "Cantidad": 3 },
-                {"Producto": "Tomate " , "Precio": 50.30, "Cantidad": 8000 },
-                {"Producto": "Zanahoria" , "Precio": 12.36 , "Cantidad": 5464 },
-                {"Producto": "Rucula" , "Precio": 10, "Cantidad": 13 }
-            ]
+otraLista = {
+1: {"Producto": "Manzana" , "Precio": 500.25 , "Cantidad": 200,"Categoria": "Verduras","Estado": True },
+2: {"Producto": "Pera" , "Precio": 54654.5654 , "Cantidad": 300,"Categoria":"Verduras","Estado":True },
+3: {"Producto": "Tomate " , "Precio": 50.30, "Cantidad": 101,"Categoria": "Verduras","Estado":True},
+4: {"Producto": "Zanahoria" , "Precio": 12.36 , "Cantidad": 502,"Categoria":"Verduras","Estado":True },
+5: {"Producto": "Rucula" , "Precio": 10, "Cantidad": 130,"Categoria":"Verduras","Estado":True }
+}
+
+
 
 os.system('cls')
 opcion = funciones.MenuOptions()
-while(opcion != 3):
+while(opcion != 7):
     if(opcion == 1):
         funciones.fagregarProds(otraLista)
         opcion = funciones.MenuOptions()
@@ -20,4 +22,20 @@ while(opcion != 3):
         os.system('cls')
         funciones.fmostrarProductos(otraLista)
         opcion = funciones.MenuOptions()
+    elif(opcion==3):
+        funciones.factualizar_Producto(otraLista)
+        opcion = funciones.MenuOptions()
+        os.system('cls')
+    elif(opcion == 4):
+        funciones.fEliminar_producto(otraLista)
+        opcion = funciones.MenuOptions()
+        os.system('cls')
+    elif(opcion == 5):
+        funciones.fBuscar_producto(otraLista)
+        opcion = funciones.MenuOptions()
+    elif(opcion == 6):
+        funciones.fReporte_bajo_stock(otraLista,100)
+        opcion = funciones.MenuOptions()
+        
+
 
